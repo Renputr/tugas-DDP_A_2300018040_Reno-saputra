42372e7eb47537eea98211394d2e73bab40aa279
Renputr <149934690+Renputr@users.noreply.github.com>
2023-11-07 09:49:39 +0700

    Add files via upload

---
+++ tugas ddp.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+	int i,j,s;
+	int hasil;
+	cout << "masukkan jumlah kotak yang akan anda buat  : ";
+	cin >> hasil;
+	
+	for(i=1; i<=1; i++){
+		for(int j=1; j<=hasil; j++){
+			cout <<"_______"<<endl;
+		
+		for(s=1; s<=3; s++){
+			cout <<"|_|_|_|"<<endl;
+		}
+	
+	}
+			cout << endl;
+		}
+			return 0;
+
+	}
+	
+
+
